4917db1d762f14bb3efc0ba60ae51bd5842c7fb9
Lucas Merckelbach <lucas.merckelbach@hzg.de>
2019-11-08 14:48:10 +0100

    Initial commit, continued from bazaar

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+from distutils.core import setup
+
+
+setup(name="coroutine",
+      version="0.1",
+      py_modules = ["coroutine"],
+      scripts = [],
+      author = "Lucas Merckelbach",
+      author_email = "lucas.merckelbach@hzg.de",
+      url = "")
